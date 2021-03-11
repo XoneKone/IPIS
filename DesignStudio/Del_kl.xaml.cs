639af985a32e8e9b7c00a48e1532967692a768f4
Julia <c.julia@rambler.ru>
2021-03-12 01:25:22 +0300

    формочки "клиенты" при нажатии на кнопку

---
+++ DesignStudio/Del_kl.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DesignStudio
+{
+    /// <summary>
+    /// Логика взаимодействия для Del_kl.xaml
+    /// </summary>
+    public partial class Del_kl : Window
+    {
+        public Del_kl()
+        {
+            InitializeComponent();
+        }
+    }
+}
